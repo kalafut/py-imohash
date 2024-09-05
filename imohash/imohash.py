33fb810f03a2abcc519404b8422334e9d6f7af48
Jim Kalafut <jim@kalafut.net>
2024-09-05 10:23:59 -0700

    Handle case when file size is too small for sample size

--- imohash/imohash.py
+++ imohash/imohash.py
@@ -12,43 +12,64 @@ import varint
 SAMPLE_THRESHOLD = 128 * 1024
 SAMPLE_SIZE = 16 * 1024
 
-#Hashes an opened file object. Compatible with paramimo SFTPFile and regular files.
-def hashfileobject(f, sample_threshhold=SAMPLE_THRESHOLD, sample_size=SAMPLE_SIZE, hexdigest=False):
-    #get file size from file object
+
+# Hashes an opened file object. Compatible with paramimo SFTPFile and regular files.
+def hashfileobject(
+    f, sample_threshhold=SAMPLE_THRESHOLD, sample_size=SAMPLE_SIZE, hexdigest=False
+):
+    # get file size from file object
     f.seek(0, os.SEEK_END)
     size = f.tell()
     f.seek(0, os.SEEK_SET)
 
-    if size < sample_threshhold or sample_size < 1:
+    if size < sample_threshhold or sample_size < 1 or size < (4 * sample_size):
         data = f.read()
     else:
-        data = f.read(sample_size)
-        f.seek(size//2)
-        data += f.read(sample_size)
+        data = read_full(f, sample_size)
+        f.seek(size // 2)
+        data += read_full(f, sample_size)
         f.seek(-sample_size, os.SEEK_END)
-        data += f.read(sample_size)
+        data += read_full(f, sample_size)
 
     hash_tmp = mmh3.hash_bytes(data)
     hash_ = hash_tmp[7::-1] + hash_tmp[16:7:-1]
     enc_size = varint.encode(size)
-    digest = enc_size + hash_[len(enc_size):]
+    digest = enc_size + hash_[len(enc_size) :]
 
     return binascii.hexlify(digest).decode() if hexdigest else digest
 
-def hashfile(filename, sample_threshhold=SAMPLE_THRESHOLD, sample_size=SAMPLE_SIZE, hexdigest=False):
-    with open(filename, 'rb') as f:
-        return hashfileobject(f, sample_threshhold, sample_size, hexdigest)
 
+# Reads exactly sample_size bytes from file object f, erroring if EOF is reached before that.
+def read_full(f, sample_size):
+    data = b""
+    while len(data) < sample_size:
+        chunk = f.read(sample_size - len(data))
+        if not chunk:
+            raise EOFError("Could not read enough data")
+        if len(chunk) == sample_size:
+            return chunk
+        data += chunk
+    return data
+
+
+def hashfile(
+    filename,
+    sample_threshhold=SAMPLE_THRESHOLD,
+    sample_size=SAMPLE_SIZE,
+    hexdigest=False,
+):
+    with open(filename, "rb") as f:
+        return hashfileobject(f, sample_threshhold, sample_size, hexdigest)
 
 
 def imosum():
     if len(sys.argv) == 1:
-        print('imosum filenames')
+        print("imosum filenames")
         return
 
     for fn in sys.argv[1:]:
         if not os.path.exists(fn):
-            print('imosum: {}: No such file or directory'.format(fn))
+            print("imosum: {}: No such file or directory".format(fn))
             continue
         if not os.path.isdir(fn):
-            print('{}  {}'.format(hashfile(fn, hexdigest=True), fn))
+            print("{}  {}".format(hashfile(fn, hexdigest=True), fn))
--- setup.py
+++ setup.py
@@ -5,59 +5,49 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='imohash',
-
+    name="imohash",
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.5',
-
-    description='Fast hashing for large files',
+    version="1.1.0",
+    description="Fast hashing for large files",
     long_description=long_description,
-
     # The project's main homepage.
-    url='https://github.com/kalafut/py-imohash',
-
+    url="https://github.com/kalafut/py-imohash",
     # Author details
-    author='Jim Kalafut',
-    author_email='jim@kalafut.net',
-
+    author="Jim Kalafut",
+    author_email="jim@kalafut.net",
     # Choose your license
-    license='MIT',
-
+    license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-
+        "Development Status :: 5 - Production/Stable",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Utilities',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.11',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.11",
     ],
-    keywords='hash hashing imohash',
+    keywords="hash hashing imohash",
     packages=find_packages(),
-    install_requires=['mmh3 >= 2.5.1',
-                      'varint >= 1.0.2'],
+    install_requires=["mmh3 >= 2.5.1", "varint >= 1.0.2"],
     entry_points={
-        'console_scripts': [
-            'imosum=imohash.imohash:imosum',
+        "console_scripts": [
+            "imosum=imohash.imohash:imosum",
         ],
     },
 )
